48d5b1c59c8c56838734ab725e553171e94dece9
zjw131f1fc <zjw5428c@gmail.com>
2026-01-13 01:58:18 +0800

    删除非batch代码

--- method/__init__.py
+++ method/__init__.py
@@ -9,22 +9,6 @@ from .models.token_merger import LearnableTokenMerger, LearnableTokenMergerV2, L
 from .models.layer_pruner import LayerSpecificPruner, VisionPrunerHead, VisionPrunerHeadSimple
 from .training import train_step
 from .evaluation import eval_step
-from .utils import (
-    get_vision_features_and_question_embedding,
-    apply_mask_to_embeddings,
-    batch_forward_with_masks,
-    extract_target_hidden_states,
-    compute_task_loss,
-    get_current_sparsity_weight,
-    update_generator_temperature,
-    get_target_token_num,
-    # Multi-layer hook utilities
-    create_layer_pruning_modifier,
-    register_multi_layer_hooks,
-    remove_hooks,
-    replace_vision_tokens_in_embeddings,
-    update_temperature_for_all
-)
 
 __all__ = [
     # Models
@@ -39,19 +23,4 @@ __all__ = [
     # Training & Evaluation
     'train_step',
     'eval_step',
-    # Utils
-    'get_vision_features_and_question_embedding',
-    'apply_mask_to_embeddings',
-    'batch_forward_with_masks',
-    'extract_target_hidden_states',
-    'compute_task_loss',
-    'get_current_sparsity_weight',
-    'update_generator_temperature',
-    'get_target_token_num',
-    # Multi-layer hook utilities
-    'create_layer_pruning_modifier',
-    'register_multi_layer_hooks',
-    'remove_hooks',
-    'replace_vision_tokens_in_embeddings',
-    'update_temperature_for_all',
 ]
